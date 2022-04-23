b6ef1a38f32903557481694586fdfad2f1270d88
Deynox1 <juanpamogon10@gmail.com>
2022-04-22 22:58:07 -0500

    Sesión 11

---
+++ test/co/edu/uco/crosscutting/util/object/UtilObjectTest.java
@@ -0,0 +1,64 @@
+package co.edu.uco.crosscutting.util.object;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import co.edu.co.crosscutting.util.object.UtilObject;
+
+class UtilObjectTest {
+
+	@Test
+	void validateIfObjectIsNull() {
+		// Arrange
+		Object value = null;
+		
+		// Act
+		boolean result = UtilObject.getUtilObject().isNull(value);
+		
+		// Assert
+		assertTrue(result);
+	}
+	
+	@Test
+	void validateIfObjectIsNotNull() {
+		// Arrange
+		Object value = new Object();
+		
+		// Act
+		boolean result = UtilObject.getUtilObject().isNull(value);
+		
+		// Assert
+		assertFalse(result);
+		
+	}
+	
+	@Test
+	void validateIfNotNullObjectIsEqualToOriginal() {
+		// Arrange
+		Object value = new Object();
+		Object defaultValue = new Object();
+		
+		// Act
+		Object result = UtilObject.getUtilObject().getDefault(value, defaultValue);
+		
+		// Assert
+		assertTrue(value.equals(result));
+		
+	}
+	
+	@Test
+	void validateIfObjectIsEqualToDefaultObjectWhenOriginalObjectIsNull() {
+		// Arrange
+		Object value = null;
+		Object defaultValue = new Object();
+		
+		// Act
+		Object result = UtilObject.getUtilObject().getDefault(value, defaultValue);
+		
+		// Assert
+		assertTrue(defaultValue.equals(result));
+		
+	}
+
+}
